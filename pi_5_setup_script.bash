e76b296a0b84f01a58daaf5d06cff81edc447402
cameronzucker <cameronzucker@gmail.com>
2025-03-31 05:38:16 -0700

    Commented out most of the functional portions while working out basic syntax.

--- pi_5_setup_script.bash
+++ pi_5_setup_script.bash
@@ -3,66 +3,66 @@ echo "AREDN Pi 5 Setup Script"
 
 # Pull and install package updates before starting
 echo "Installing updates..."
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt update
-sudo apt upgrade
+apt-get update
+apt-get upgrade -y
+apt update
+apt upgrade
 
 # Install necessary background packages/dependencies
-echo "Installing required packages..."
-sudo bash -c 'apt-get -y install npm git gpsd gpsd-clients chrony >/dev/null 2>&1 & disown'
+#echo "Installing required packages..."
+#sudo bash -c 'apt-get -y install npm git gpsd gpsd-clients chrony >/dev/null 2>&1 & disown'
 
 # Disable USB port power limits
-echo "Configuring USB port power limits..."
-sudo nano /boot/firmware/config.txt
-need to add "usb_max_current_enable=1" under [all]
+#echo "Configuring USB port power limits..."
+#sudo nano /boot/firmware/config.txt
+#need to add "usb_max_current_enable=1" under [all]
 
 # Install RealVNC components
-echo "Installing RealVNC..."
-sudo bash -c 'apt-get -y install realvnc-vnc-server realvnc-vnc-viewer >/dev/null 2>&1 & disown'
+#echo "Installing RealVNC..."
+#sudo bash -c 'apt-get -y install realvnc-vnc-server realvnc-vnc-viewer >/dev/null 2>&1 & disown'
 
 # Install chirp
-echo "Installing CHIRP..."
-sudo bash -c 'apt-get -y install chirp >/dev/null 2>&1 & disown'
+#echo "Installing CHIRP..."
+#sudo bash -c 'apt-get -y install chirp >/dev/null 2>&1 & disown'
 
 # Install librewolf
-echo "Installing Librewolf browser..."
-sudo bash -c 'apt-get -y install librewolf >/dev/null 2>&1 & disown'
+#echo "Installing Librewolf browser..."
+#sudo bash -c 'apt-get -y install librewolf >/dev/null 2>&1 & disown'
 
 # Install patwinlink (additional config needed later)
-echo "Installing PAT Winlink..."
-cd ~
-sudo apt install wget libhamlib-utils screen
-sudo wget https://github.com/la5nta/pat/releases/download/v0.16.0/pat_0.16.0_linux_arm64.deb
-sudo dpkg -i pat_0.16.0_linux_arm64.deb
+#echo "Installing PAT Winlink..."
+#cd ~
+#sudo apt install wget libhamlib-utils screen
+#sudo wget https://github.com/la5nta/pat/releases/download/v0.16.0/pat_0.16.0_linux_arm64.deb
+#sudo dpkg -i pat_0.16.0_linux_arm64.deb
 
 # Install docker per official docs: https://docs.docker.com/engine/install/ubuntu/
-echo "Removing old/conflicting Docker packages..."
-sudo bash -c 'for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done'
+#echo "Removing old/conflicting Docker packages..."
+#sudo bash -c 'for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done'
 
 # Add Docker's official GPG key:
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+#sudo apt-get install ca-certificates curl
+#sudo install -m 0755 -d /etc/apt/keyrings
+#sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+#sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+#echo \
+#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+#  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+#  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo apt-get update
 
 # Install docker components
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Change to /srv/ for Docker container installs
-cd /
-cd srv
+#cd /
+#cd srv
 
 # Make Docker container folders
-sudo mkdir filebrowser mirotalk openspeedtest openwebrx+ statping
+#sudo mkdir filebrowser mirotalk openspeedtest openwebrx+ statping
 
 # Edit config.txt file to enable PCIe 3.0, then reboots to affect change
-Add somehow...dtparam=pciex1_gen=3
-sudo shutdown -r now
+#Add somehow...dtparam=pciex1_gen=3
+#sudo shutdown -r now
